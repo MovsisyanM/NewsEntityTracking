53fe01aea6fa04bd58f836b80af858984745ebd0
Movsisyan <movsisyan@protonmail.com>
2024-03-04 02:03:22 +0400

    File separation sync 1

--- scraper/orchestrator.py
+++ scraper/orchestrator.py
@@ -18,29 +18,88 @@ import yaml
 
 config_path = "config.yaml"
 
-with open(config_path, "w") as f:
-    yaml.dump({
-        "redis": {
-            "host": "localhost",
-            "port": 5211
-        },
-        "sqlite": {
-            "path": "scraping.db"
-        }
-    }, f)
+config = yaml.load(config_path)
 
-# config = yaml.load(config_path)
-# print(config)
+db_options = {
+    "sqlite": lambda config: sqlite3.connect(**config["sqlite"])
+}
 
-# r = redis.Redis("localhost")
+cache_options = {
+    "redis": lambda config: redis.Redis(**config["redis"])
+}
+
+
+def create_article_table_sqlite(
+        db: sqlite3.Connection,
+        config: dict
+):
+    """Creates tables to store the articles in for all sites
+
+    Args:
+        db (sqlite3.Connection): sqlite connection to the database.
+        config (dict): configuration dictionary with `sites` key and
+        dict value where each key is a site and each value is a dict
+        of column names and their properties.
+    """
+
+    # db.execute("drop table if exists article")
+    db.commit()
+
+    # for each news website
+    for site, pairs in config["sites"].items():
+        sql_command = "create table if not exists article_"
+        sql_command += site + " (\n"
+
+        cols = []
+        # for each table column
+        for key, value in pairs.items():
+            col = "\t" + key + " " + value
+            col += ",\n" if len(cols) - 1 < len(pairs) else ");"
+
+    db.commit()
 
 # endregion init
 
 
 # 1. Date sequence generator
 
+# newsam
+    
+initial_date = datetime.datetime(2009, 6, 25)
+on_date = initial_date
+
+while on_date <= datetime.datetime.now():
+    print(on_date.year, end="\r")
+    r.lpush("to_fetch_list_newsam", f"{on_date.year}/{on_date.month}/{on_date.day}/")
+    on_date += datetime.timedelta(1)
 
 
+# tertam
+    
+initial_date = datetime.datetime(2010, 3, 2)
+on_date = initial_date
+
+while on_date <= datetime.datetime.now():
+    print(on_date.year, end="\r")
+    r.lpush("to_fetch_list_tertam", on_date.strftime("%Y/%m/%d"))
+    on_date += datetime.timedelta(1)
+
+
+# armenpress
+    
+lastid = 3279
+
+while True:
+    article_ids, timestamps = get_article_list(lastid)
+
+    if len(article_ids) == 0:
+        break
+    lastid += 1
+
+    [insert_article(article_ids[i], None, timestamps[i], None, None, False) for i in range(len(article_ids))]
+    db.commit()
+    # time.sleep(0.15)
+    print(lastid, end="\r")
 
 # 2. Article info storer
 
